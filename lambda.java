41f529f3dc67c896e24861a72b8887b353e8ca48
Mário <mariobalen3@gmail.com>
2025-03-07 22:17:54 -0300

    Lista original

---
+++ lambda.java
@@ -0,0 +1,8 @@
+import java.util.List;
+
+public class lambda {
+    public static void main(String[] args) {
+        List<Integer>numbers = List.of(10,8,9,7,6,3,4,5,2,1);
+        System.out.println("Lista original: " + numbers);
+    }
+}
