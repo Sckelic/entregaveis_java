1a8ce17ce4502a029bc65cbb6f3be423f5552d51
Mário <mariobalen3@gmail.com>
2025-03-07 22:22:51 -0300

    Filtro por numeros pares

--- lambda.java
+++ lambda.java
@@ -1,8 +1,13 @@
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class lambda {
     public static void main(String[] args) {
         List<Integer>numbers = List.of(10,8,9,7,6,3,4,5,2,1);
         System.out.println("Lista original: " + numbers);
+        System.out.println();
+        System.out.println("Filtro: ");
+        List<Integer>pares = numbers.stream().filter(n -> n % 2 == 0).collect(Collectors.toList());
+        System.out.println("Numeros pares: " + pares);
     }
 }
